f020be9eb96f2ad6216f6a50b5be3a791ed0f549
lophey <laprop34@gmail.com>
2022-06-30 23:46:17 +0300

    First Commit

---
+++ Main/views.py
@@ -0,0 +1,21 @@
+import requests
+from django.shortcuts import render
+import datetime as dt
+
+
+def index(request):
+    appid = '053c98c9c0a3368f0e8cd2db240f3c91'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&exclude=current&lang=ua&appid=' + appid
+    city = 'Измаил'
+    res = requests.get(url.format(city)).json()
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"],
+        'current_time': dt.datetime.utcfromtimestamp(res['dt'] + res['timezone'])
+    }
+
+    context = {'info': city_info}
+
+    return render(request, 'Main/index.html', context)
