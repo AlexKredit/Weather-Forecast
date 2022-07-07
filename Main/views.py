1193155219aee9b084c7a2ee7ef5a0733f17836f
lophey <laprop34@gmail.com>
2022-07-08 01:09:03 +0300

    Final push?

--- Main/views.py
+++ Main/views.py
@@ -20,16 +20,18 @@ def get_queryset(request):
 
 def index(request):
     base_url_current = 'http://api.openweathermap.org/data/2.5/weather?q='
-    base_url_forecast = 'https://api.openweathermap.org/data/2.5/forecast?q='
+
     appid = '053c98c9c0a3368f0e8cd2db240f3c91'
     lang = "en"
 
     url_current = base_url_current + '{}&units=metric&exclude=current' + '&lang=' + lang + '&appid=' + appid
-    url_forecast = base_url_forecast + '{}&units=metric&exclude=hourly&cnt=8' + \
-                   '&lang=' + lang + '&appid=' + appid
 
     for city in get_queryset(request):
         res_current = requests.get(url_current.format(city)).json()
+        lat = str(res_current['coord']['lat'])
+        lon = str(res_current['coord']['lon'])
+        base_url_forecast = 'https://api.openweathermap.org/data/2.5/onecall'
+        url_forecast = base_url_forecast + '?lat=' + lat + '&lon=' + lon + '&units=metric&exclude=minutely,hourly,alerts&cnt=8' + '&lang=' + lang + '&appid=' + appid
         res_forecast = requests.get(url_forecast.format(city)).json()
         current_time = dt.datetime.utcfromtimestamp(res_current['dt'] + res_current['timezone'])
         current_time_str = current_time.strftime('%H:%M')
@@ -43,6 +45,8 @@ def index(request):
             'country': res_current['sys']['country'],
             'temp': toFixed(res_current["main"]["temp"]),
             'temp_feels_like': toFixed(res_current['main']['feels_like']),
+            'temp_max': toFixed(res_current['main']['temp_max']),
+            'temp_min': toFixed(res_current['main']['temp_min']),
             'wind_speed': toFixed(res_current['wind']['speed'], 1),
             'weatherType': res_current["weather"][0]["main"],
             'weatherTypeFull': res_current["weather"][0]["description"],
@@ -52,15 +56,46 @@ def index(request):
             'current_time': current_time_str,
             'current_date': current_date_str,
             'sunrise_time': sunrise_time_str,
-            'sunset_time': sunset_time_str
+            'sunset_time': sunset_time_str,
+            'cloudness': res_current['clouds']['all']
         }
-        A = []
-
-        for i in res_forecast['list']:
-            A += toFixed(i['main']['temp'])
+        tempMax_forecast = []
+        tempMin_forecast = []
+        day_forecast = []
+        icon_forecast = []
+        weather_forecast = []
+        for i in range(6):
+            tempMax_forecast.append(res_forecast['daily'][i]['temp']['max'])
+            tempMin_forecast.append(res_forecast['daily'][i]['temp']['min'])
+            day_forecast.append(dt.datetime.utcfromtimestamp(res_forecast['daily'][i]['dt'] + res_forecast['timezone_offset']))
+            icon_forecast.append(res_forecast['daily'][i]['weather'][0]['icon'])
+            weather_forecast.append(res_forecast['daily'][i]['weather'][0]['main'])
         city_forecast_info = {
-            'temp': toFixed(res_forecast["list"][0]["main"]["temp"]),
-             'wind_forecast': A
+            'temp_Max1': toFixed(tempMax_forecast[1]),
+            'temp_Max2': toFixed(tempMax_forecast[2]),
+            'temp_Max3': toFixed(tempMax_forecast[3]),
+            'temp_Max4': toFixed(tempMax_forecast[4]),
+            'temp_Max5': toFixed(tempMax_forecast[5]),
+            'temp_Min1': toFixed(tempMin_forecast[1]),
+            'temp_Min2': toFixed(tempMin_forecast[2]),
+            'temp_Min3': toFixed(tempMin_forecast[3]),
+            'temp_Min4': toFixed(tempMin_forecast[4]),
+            'temp_Min5': toFixed(tempMin_forecast[5]),
+            'day1': day_forecast[1].strftime('%b, %a %d'),
+            'day2': day_forecast[2].strftime('%b, %a %d'),
+            'day3': day_forecast[3].strftime('%b, %a %d'),
+            'day4': day_forecast[4].strftime('%b, %a %d'),
+            'day5': day_forecast[5].strftime('%b, %a %d'),
+            'icon1': icon_forecast[1],
+            'icon2': icon_forecast[2],
+            'icon3': icon_forecast[3],
+            'icon4': icon_forecast[4],
+            'icon5': icon_forecast[5],
+            'weather1': weather_forecast[1],
+            'weather2': weather_forecast[2],
+            'weather3': weather_forecast[3],
+            'weather4': weather_forecast[4],
+            'weather5': weather_forecast[5]
         }
         context = {'info': city_info, 'info_forecast': city_forecast_info}
         return render(request, 'Main/index.html', context)
